f0f7681840724c0eeceabf757e36368f21b25318
sardap <pfsarda23@gmail.com>
2020-07-21 05:44:26 +1000

    inital commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/sardap/discgov
+
+go 1.14
+
+require (
+	github.com/bwmarrin/discordgo v0.21.1
+	github.com/orcaman/concurrent-map v0.0.0-20190826125027-8c72a8bb44f6 // indirect
+	github.com/vmihailenco/taskq/v3 v3.0.0-beta.11
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package pkg
+
+import (
+	"sync"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type state int
+
+type guildInfo struct {
+	usersLoc sync.Map
+}
+
+var guildInfos sync.Map
+var stLock = sync.RWMutex{}
+
+func init() {
+}
+
+//GetUsers Returns a list of users in a channel
+func GetUsers(guildID, channelID string) []string {
+	gInfoTmp, ok := guildInfos.Load(guildID)
+	if !ok {
+		return nil
+	}
+	gInfo := gInfoTmp.(*guildInfo)
+	result := make([]string, 0)
+	gInfo.usersLoc.Range(func(channel, user interface{}) bool {
+		if channel.(string) == channelID {
+			result = append(result, user.(string))
+		}
+
+		return true
+	})
+	return result
+}
+
+//UserVoiceTrackerHandler this is the handler which must be added as a handler to discrodgo it will update the
+// Staes whenever a user moves between channels
+func UserVoiceTrackerHandler(s *discordgo.Session, v *discordgo.VoiceStateUpdate) {
+	gInfoTmp, _ := guildInfos.LoadOrStore(v.GuildID, &guildInfo{})
+	gInfo := gInfoTmp.(*guildInfo)
+	//Left Channel
+	if v.ChannelID == "" {
+		_, ok := gInfo.usersLoc.Load(v.UserID)
+		if !ok {
+			return
+		}
+		gInfo.usersLoc.Delete(v.UserID)
+	} else {
+		lastUserCh, ok := gInfo.usersLoc.Load(v.UserID)
+		//First time user has been seen or user is moving between channels
+		if !ok || lastUserCh.(string) != v.ChannelID {
+			gInfo.usersLoc.Store(v.UserID, v.ChannelID)
+		}
+	}
+}
